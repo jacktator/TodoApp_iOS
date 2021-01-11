c113f6e71b6248a70a2903cb09c294f567502396
Jack Song <jack.song@lendi.com.au>
2021-01-12 10:50:23 +1100

    Initial Commit

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Jack Song on 12/1/21.
+//  Copyright © 2021 Jack Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
